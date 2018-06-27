2f57ac4d574a5f5a6941ecb7c0ba2253e6c962a9
William Dale <williambdale@gmail.com>
2018-06-27 17:56:50 -0400

    Add CRUD to location

--- app/controllers/api/v1/location_controller.rb
+++ app/controllers/api/v1/location_controller.rb
@@ -2,6 +2,32 @@ module Api
   module V1
     class LocationController < ApplicationController
 
+      def index
+        locations = Location.all
+        render json: {status: 'SUCCESS', message: 'Loaded Locations', data: locations}, status: :ok
+      end
+
+      def show
+        location = Location.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded Location', data: location}, status: :ok
+      end
+
+      def create
+        location = Location.new(location_params)
+
+        if location.save
+          render json: {status: 'SUCCESS', message: 'Loaded Location', data: location}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Location Not Saved', data: location.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def location_params
+        params.permit(:user_id, :good_id, :date_needed, :date_locationed, :priority)
+      end
+
     end
   end
 end

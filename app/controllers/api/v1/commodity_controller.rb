b078511f26d972df84861386367786ba9e10c6dd
William Dale <williambdale@gmail.com>
2018-06-27 17:47:08 -0400

    Add CRUD to commodity

--- app/controllers/api/v1/commodity_controller.rb
+++ app/controllers/api/v1/commodity_controller.rb
@@ -4,23 +4,38 @@ module Api
 
       def index
         commodities = Commodity.all
-        render json {status: 'SUCCESS', message: 'Loaded Commodities', data:commodities}, status: :ok
+        render json: {status: 'SUCCESS', message: 'Loaded Commodities', data: commodities}, status: :ok
       end
 
       def show
-
+        commodity = Commodity.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded Commodity', data: commodity}, status: :ok
       end
 
       def create
+        commodity = Commodity.new(commodity_params)
 
+        if commodity.save
+          render json: {status: 'SUCCESS', message: 'Saved Commodity', data: commodity}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Commodity Not Saved', data: commodity.errors}, status: :unprocessable_entity
+        end
       end
 
       def update
+        commodity = Commodity.find(params[:id])
 
+        if commodity.update
+          render json: {status: 'SUCCESS', message: 'Updated Commodity', data: commodity}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Commodity Not Updated', data: commodity.errors}, status: :unprocessable_entity
+        end
       end
 
       def destroy
-
+        commodity = Commodity.find(params[:id])
+        commodity.destroy
+        render json: {status: 'SUCCESS', message: 'Deleted Commodity', data: commodity}, status: :ok
       end
 
 

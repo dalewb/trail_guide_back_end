ad9d732ff5a3d7d7d00184a6d0e54c175ab102de
William Dale <williambdale@gmail.com>
2018-07-06 07:37:55 -0400

    Add location id to post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_141944) do
+ActiveRecord::Schema.define(version: 2018_07_06_112615) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "user_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_07_03_141944) do
     t.integer "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "location_id"
   end
 
   create_table "users", force: :cascade do |t|
